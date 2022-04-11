a8c8b6cda87bd639e546387e48aa3a502ee8fc84
Rodrigo Alfonso <rodri042@gmail.com>
2022-04-11 20:05:56 -0300

    Deploy @ 11/04/2022

---
+++ precache-manifest.ce20d83e0a8723077ecf5d5ac4d1b36e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b31dca02d2104f9a4731",
+    "url": "/nestation/static/css/main.1d8f110e.chunk.css"
+  },
+  {
+    "revision": "b31dca02d2104f9a4731",
+    "url": "/nestation/static/js/main.a32b5951.chunk.js"
+  },
+  {
+    "revision": "7d8307fa7ee481194da1",
+    "url": "/nestation/static/js/runtime~main.f5c8d64f.js"
+  },
+  {
+    "revision": "00208fd4d0a79aa6f8d5",
+    "url": "/nestation/static/css/2.71be3bfd.chunk.css"
+  },
+  {
+    "revision": "00208fd4d0a79aa6f8d5",
+    "url": "/nestation/static/js/2.a82b095f.chunk.js"
+  },
+  {
+    "revision": "2e5e0dc316125d992caa1ab598efc47e",
+    "url": "/nestation/static/media/halu.2e5e0dc3.svg"
+  },
+  {
+    "revision": "fcf23740e4bcb19747d390775e401883",
+    "url": "/nestation/index.html"
+  }
+];
